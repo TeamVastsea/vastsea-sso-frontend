67c8d2c9a6872cfbe48efe7cc72b885e8b12cda0
GaoNeng-wWw <gaonenggg@outlook.com>
2025-03-31 09:20:11 +0800

    feat(swagger): add swagger doc

--- global.d.ts
+++ global.d.ts
@@ -2,5 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     REDIS_CLUSTER?: boolean;
+    SWAGGER_TITLE?: string;
+    SWAGGER_DESC?: string;
+    VERSION?: string;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(process.env.SWAGGER_DESC)
+    .setVersion(process.env.ERSION)
+    .build();
+  const factory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/swagger', app, factory);
+  patchNestJsSwagger();
   await app.listen(3000);
 }
 bootstrap();
