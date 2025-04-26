36dbfdf6853d28dedd85cb4f79f54127113395fa
GaoNeng-wWw <gaonenggg@outlook.com>
2025-04-26 21:10:42 +0800

    fix: home redirect auth page

--- packages/frontend/src/router/guards/index.ts
+++ packages/frontend/src/router/guards/index.ts
@@ -1,7 +1,8 @@
 import type { Router } from 'vue-router';
 import authGuard from './auth-guard';
+import autoTitle from './auto-title';
 
-const guards = [authGuard];
+const guards = [authGuard, autoTitle];
 
 export function setupGuard(router: Router) {
   guards.forEach(guard => guard(router));
