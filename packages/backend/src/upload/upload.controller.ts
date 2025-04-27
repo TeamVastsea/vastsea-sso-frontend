49cdc6aca4a91dd781b6e2a38a557b90d5d2d653
GaoNeng-wWw <gaonenggg@outlook.com>
2025-04-27 21:50:30 +0800

    feat(upload): upload profile/client avatar

--- packages/backend/src/upload/upload.controller.ts
+++ packages/backend/src/upload/upload.controller.ts
@@ -1,7 +1,83 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { ConfigService } from '@app/config';
+import { Account, Auth, BigIntPipe, Permission } from '@app/decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ProfileService } from '../profile/profile.service';
+import { ClientService } from '../client/client.service';
 
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
+  constructor(
+    private readonly uploadService: UploadService,
+    private readonly config: ConfigService,
+    private readonly profileService: ProfileService,
+    private readonly clientService: ClientService,
+  ) {}
+
+  @Auth()
+  @Post('/profile/avatar')
+  @UseInterceptors(
+    FileInterceptor('profile', { limits: { files: 1, fileSize: 2000000 } }),
+  )
+  async uploadProfileAvatar(
+    @Account('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const _id = BigInt(id);
+    const hash = this.uploadService.getFileHash(file.buffer);
+    const path = this.uploadService.resolvePath(
+      this.config.get('file.avatar.profile.path') ?? `/public/${__dirname}`,
+      file.filename,
+    );
+    await this.uploadService.storageFile(
+      this.uploadService.resolveStorageParam(file, path, hash),
+    );
+    await this.profileService.updateProfile(_id, {
+      avatar: path,
+    });
+    return hash;
+  }
+
+  @Permission(['UPDATE::CLIENT'])
+  @Auth()
+  @Post('')
+  @UseInterceptors(
+    FileInterceptor('image', { limits: { files: 1, fileSize: 2000000 } }),
+  )
+  async uploadClientAvatar(
+    @Query('id', BigIntPipe) id: bigint,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const hash = this.uploadService.getFileHash(file.buffer);
+    const path = this.uploadService.resolvePath(__dirname, `/public/image`);
+    const clientAvatarPath = await this.uploadService
+      .storageFile(this.uploadService.resolveStorageParam(file, path, hash))
+      .then((hash) => {
+        return `/image/${hash}`;
+      });
+    await this.clientService.updateClient(
+      id,
+      {
+        avatar: clientAvatarPath,
+      },
+      -1n,
+      true,
+    );
+  }
+
+  @Permission(['REMOVE::IMAGE::*'])
+  @Auth()
+  @Delete(':hash')
+  async removeImage(@Param('hash') hash: string) {
+    return this.uploadService.removeFile(hash);
+  }
 }
--- packages/backend/src/upload/upload.service.ts
+++ packages/backend/src/upload/upload.service.ts
@@ -1,4 +1,102 @@
-import { Injectable } from '@nestjs/common';
+import { AutoRedis } from '@app/decorator';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { createHash } from 'crypto';
+import { promises } from 'dns';
+import { unlinkSync, writeFileSync } from 'fs';
+import Redis, { Cluster } from 'ioredis';
+import { join } from 'path';
+import sharp from 'sharp';
 
+const FILE_META = (fileHash: string) => `FILE::${fileHash}::REF::COUNTER`;
+
+type FileMeta = {
+  ref: number;
+  path: string;
+};
+type StorageFile = {
+  file: Buffer;
+  path: string;
+  hash: string;
+  size: [number, number];
+};
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  constructor(@AutoRedis() private redis: Redis | Cluster) {}
+
+  async storageFile({ file, path, hash, size }: StorageFile) {
+    const key = FILE_META(hash);
+    const fileMeta = (await this.redis.hgetall(
+      key,
+    )) as unknown as FileMeta | null;
+    if (fileMeta) {
+      await this.redis.hincrby(key, 'ref', 1);
+      return fileMeta.path;
+    }
+    return sharp(file)
+      .resize(size[0], size[1], { fit: 'inside' })
+      .webp({ quality: 95, nearLossless: true })
+      .toBuffer()
+      .then((buf) => {
+        return writeFileSync(path, buf);
+      })
+      .then(() => {
+        return this.redis
+          .hmset(key, {
+            path,
+            ref: 0,
+          } satisfies FileMeta)
+          .then(() => hash);
+      });
+  }
+
+  async refFile(fileHash: string) {
+    const fileMeta = await this.redis.hgetall(FILE_META(fileHash));
+    if (!fileMeta) {
+      throw new HttpException('文件不存在', HttpStatus.NOT_FOUND);
+    }
+    await this.redis.hincrby(FILE_META(fileHash), 'ref', 1);
+    return {
+      ...fileMeta,
+      ref: fileMeta.ref + 1,
+    };
+  }
+  async unrefFile(fileHash: string) {
+    const fileMeta = await this.getFileMeta(fileHash);
+    if (!fileMeta) {
+      throw new HttpException('文件不存在', HttpStatus.NOT_FOUND);
+    }
+    await this.redis.hincrby(FILE_META(fileHash), 'ref', -1);
+    if (fileMeta.ref - 1 === 0) {
+      unlinkSync(fileMeta.path);
+    }
+    return true;
+  }
+  async removeFile(fileHash: string) {
+    const fileMeta = await this.getFileMeta(fileHash);
+    if (!fileMeta) {
+      throw new HttpException('文件不存在', HttpStatus.NOT_FOUND);
+    }
+    await this.redis.hset(FILE_META(fileHash), 'ref', 0);
+    unlinkSync(fileMeta.path);
+  }
+  getFileMeta(fileHash: string) {
+    return this.redis.hgetall(
+      FILE_META(fileHash),
+    ) as unknown as Promise<FileMeta | null>;
+  }
+
+  getFileHash(file: Buffer) {
+    const hash = createHash('sha256');
+    return hash.update(file).digest('base64url');
+  }
+  resolvePath(basePath: string, fileName: string) {
+    return join(basePath, fileName);
+  }
+  resolveStorageParam(
+    file: Express.Multer.File,
+    path: string,
+    hash: string,
+  ): StorageFile {
+    return { file: file.buffer, path, hash, size: [200, 200] };
+  }
+}
