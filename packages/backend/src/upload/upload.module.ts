c332f18297cadcadc74de8dbaef191a4356b9fce
GaoNeng-wWw <gaonenggg@outlook.com>
2025-04-27 21:50:30 +0800

    feat(upload): upload profile/client avatar

--- packages/backend/src/upload/upload.module.ts
+++ packages/backend/src/upload/upload.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ProfileModule } from '../profile/profile.module';
 
 @Module({
   controllers: [UploadController],
   providers: [UploadService],
+  imports: [ProfileModule],
 })
 export class UploadModule {}
