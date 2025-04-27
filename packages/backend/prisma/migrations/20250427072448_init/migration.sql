94b66ac28ec3c63369c6802fccf69d83be3913d6
GaoNeng-wWw <gaonenggg@outlook.com>
2025-04-27 15:25:43 +0800

    fix: ci

---
+++ packages/backend/prisma/migrations/20250427072448_init/migration.sql
@@ -0,0 +1,129 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL,
+    "salt" TEXT NOT NULL,
+    "iterations" INTEGER NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nick" TEXT NOT NULL,
+    "desc" TEXT,
+    "avatar" TEXT,
+    "accountid" BIGINT NOT NULL,
+    CONSTRAINT "Profile_accountid_fkey" FOREIGN KEY ("accountid") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "desc" TEXT,
+    "avatar" TEXT,
+    "clientId" TEXT NOT NULL,
+    "clientSecret" TEXT NOT NULL,
+    "redirect" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true
+);
+
+-- CreateTable
+CREATE TABLE "Permission" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "clientId" TEXT NOT NULL,
+    "clientPK" BIGINT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    CONSTRAINT "Permission_clientPK_fkey" FOREIGN KEY ("clientPK") REFERENCES "Client" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" BIGINT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "desc" TEXT NOT NULL DEFAULT '',
+    "clientId" TEXT NOT NULL,
+    "clientPK" BIGINT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    CONSTRAINT "Role_clientPK_fkey" FOREIGN KEY ("clientPK") REFERENCES "Client" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_AccountToRole" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL,
+    CONSTRAINT "_AccountToRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_AccountToRole_B_fkey" FOREIGN KEY ("B") REFERENCES "Role" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_AccountToClient" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL,
+    CONSTRAINT "_AccountToClient_A_fkey" FOREIGN KEY ("A") REFERENCES "Account" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_AccountToClient_B_fkey" FOREIGN KEY ("B") REFERENCES "Client" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_PermissionToRole" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL,
+    CONSTRAINT "_PermissionToRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Permission" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_PermissionToRole_B_fkey" FOREIGN KEY ("B") REFERENCES "Role" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_roles" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL,
+    CONSTRAINT "_roles_A_fkey" FOREIGN KEY ("A") REFERENCES "Role" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_roles_B_fkey" FOREIGN KEY ("B") REFERENCES "Role" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE INDEX "Account_email_idx" ON "Account"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_accountid_key" ON "Profile"("accountid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_clientId_key" ON "Client"("clientId");
+
+-- CreateIndex
+CREATE INDEX "Client_name_clientId_active_idx" ON "Client"("name", "clientId", "active");
+
+-- CreateIndex
+CREATE INDEX "Permission_name_clientId_idx" ON "Permission"("name", "clientId");
+
+-- CreateIndex
+CREATE INDEX "Role_id_desc_clientId_active_idx" ON "Role"("id", "desc", "clientId", "active");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_AccountToRole_AB_unique" ON "_AccountToRole"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_AccountToRole_B_index" ON "_AccountToRole"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_AccountToClient_AB_unique" ON "_AccountToClient"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_AccountToClient_B_index" ON "_AccountToClient"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PermissionToRole_AB_unique" ON "_PermissionToRole"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PermissionToRole_B_index" ON "_PermissionToRole"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_roles_AB_unique" ON "_roles"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_roles_B_index" ON "_roles"("B");
