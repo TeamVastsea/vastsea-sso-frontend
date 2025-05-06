d700b2d196637cd68d6a3b5968e2ae44ef02536c
unknown <gaonenggg@outlook.com>
2025-05-06 15:53:06 +0800

    feat: winston log

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -5,9 +5,59 @@ import { patchNestJsSwagger } from 'nestjs-zod';
 import * as cookieParser from 'cookie-parser';
 import { static as static_ } from 'express';
 import { join } from 'path';
+import 'winston-daily-rotate-file';
+import * as winston from 'winston';
+import { utilities, WinstonModule } from 'nest-winston';
+import { windowTime } from 'rxjs';
+import { FileTypeValidator } from '@nestjs/common';
+
+const CONSOLE_TRANSPORT = new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    utilities.format.nestLike(),
+  ),
+});
+
+const FILE_TRANSPORT = new winston.transports.DailyRotateFile({
+  dirname: join(__dirname, 'logs'),
+  filename: 'application-%DATE%.info.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.json(),
+  ),
+  level: 'info',
+});
+
+const ERR_FILE_TRANSPORT = new winston.transports.DailyRotateFile({
+  dirname: join(__dirname, 'logs'),
+  filename: 'application-%DATE%.err.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.json(),
+  ),
+  level: 'error',
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const instance = winston.createLogger({
+    transports: [CONSOLE_TRANSPORT, FILE_TRANSPORT, ERR_FILE_TRANSPORT],
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({ instance }),
+  });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle(process.env.SWAGGER_TITLE)
