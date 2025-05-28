57bf8242ee1659b634da2ad1bb5d12b5237fbc1a
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-28 14:31:48 +0800

    fix: logout

--- src/composables/useAccount.ts
+++ src/composables/useAccount.ts
@@ -99,7 +99,7 @@ export function useAccount(
       .then(data => (account.value = data));
   };
   const logout = () => {
-    return fetcher.delete('/account/logout');
+    return fetcher.delete('/auth/token');
   };
   return {
     formData,
