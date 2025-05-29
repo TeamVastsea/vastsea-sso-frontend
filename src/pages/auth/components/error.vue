d3c7a222175e89903969081c25418f552a6fea1f
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-29 15:43:46 +0800

    feat: take me leave button style

--- src/pages/auth/components/error.vue
+++ src/pages/auth/components/error.vue
@@ -18,7 +18,7 @@ const router = useRouter();
       </p>
     </div>
     <button
-      class="text-zinc-800 px-3 py-1.5 rounded-md w-fit cursor-pointer transition dark:text-zinc-200 hover:bg-zinc-200 hover:dark:bg-zinc-800"
+      class="text-zinc-800 px-3 py-1.5 rounded-md bg-zinc-200 w-fit cursor-pointer transition dark:text-zinc-200 dark:bg-zinc-800 hover:bg-zinc-300 hover:dark:bg-zinc-700"
       @click="router.back()"
     >
       带我离开
