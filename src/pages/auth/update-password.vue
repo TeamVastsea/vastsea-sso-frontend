a123a274038b0f4ba3f4dd43cfb3749b754e95bb
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-31 20:10:02 +0800

    fix: goto auth page when update password success

--- src/pages/auth/update-password.vue
+++ src/pages/auth/update-password.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
 import updatePassword from '@/components/update-password.vue';
+import { useAccountStore } from '@/store';
+import { useRouter } from 'vue-router';
+
+const { clearTokenPair } = useAccountStore();
+const router = useRouter();
+
+const onUpdateSuccess = () => {
+  clearTokenPair();
+  router.replace({ path: '/' });
+};
 </script>
 
 <template>
   <div class="p-4 bg-zinc-100 size-full dark:bg-zinc-900">
-    <update-password />
+    <update-password @sucess="onUpdateSuccess" />
   </div>
 </template>
